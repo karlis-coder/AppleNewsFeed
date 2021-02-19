4c1625f44107623924cfe5d5d56a4842bcfdd4ce
Karlis B <karlis.butins@gmail.com>
2021-02-19 18:31:18 +0200

    first commit

---
+++ AplleNewsFeed/Controllers/NewsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewsTableViewCell.swift
+//  AplleNewsFeed
+//
+//  Created by Karlis Butins on 15/02/2021.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsImageView: UIImageView!
+    
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    
+}
